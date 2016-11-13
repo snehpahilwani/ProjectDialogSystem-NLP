82feb1aca6dedeb920d1de6c3448b3399a83b750
Prashant Mishra <prashant24.juit@gmail.com>
2016-11-13 17:14:33 -0500

    Fix: read from existing db

--- samplecode/list_ui/app/src/main/java/com/dialogGator/DBHelper.java
+++ samplecode/list_ui/app/src/main/java/com/dialogGator/DBHelper.java
@@ -1,5 +1,10 @@
 package com.dialogGator;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.lang.reflect.Field;
 import java.util.*;
 
@@ -44,6 +49,11 @@ public class DBHelper extends SQLiteOpenHelper
             DB_PATH = "/data/data/" + context.getPackageName() + "/databases/";
         }
         this.mContext = context;
+        try {
+            createDataBase();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public ArrayList<Product> Query(Map<String, String> searchBox) {
@@ -97,8 +107,6 @@ public class DBHelper extends SQLiteOpenHelper
     {
         try
         {
-            boolean isDbFound = this.openDataBase();
-            this.close();
             return this.getReadableDatabase().rawQuery(sql, null);
             //Cursor mCur = mDataBase.rawQuery(sql, null);
             //return mCur;
@@ -160,6 +168,53 @@ public class DBHelper extends SQLiteOpenHelper
     private String NewWhereClause(String whereClause)
     {
         return whereClause.equals("") ? " where " : " and ";
+    }
+
+    public void createDataBase() throws IOException
+    {
+        //If the database does not exist, copy it from the assets.
+
+        boolean mDataBaseExist = checkDataBase();
+        if(!mDataBaseExist)
+        {
+            this.getReadableDatabase();
+            this.close();
+            try
+            {
+                //Copy the database from assests
+                copyDataBase();
+                Log.e(TAG, "createDatabase database created");
+            }
+            catch (IOException mIOException)
+            {
+                throw new Error("ErrorCopyingDataBase");
+            }
+        }
+    }
+
+    //Check that the database exists here: /data/data/your package/databases/Da Name
+    private boolean checkDataBase()
+    {
+        File dbFile = new File(DB_PATH + DB_NAME);
+        //Log.v("dbFile", dbFile + "   "+ dbFile.exists());
+        return dbFile.exists();
+    }
+
+    //Copy the database from assets
+    private void copyDataBase() throws IOException
+    {
+        InputStream mInput = mContext.getAssets().open(DB_NAME);
+        String outFileName = DB_PATH + DB_NAME;
+        OutputStream mOutput = new FileOutputStream(outFileName);
+        byte[] mBuffer = new byte[1024];
+        int mLength;
+        while ((mLength = mInput.read(mBuffer))>0)
+        {
+            mOutput.write(mBuffer, 0, mLength);
+        }
+        mOutput.flush();
+        mOutput.close();
+        mInput.close();
     }
 
     //Open the database, so we can query it
